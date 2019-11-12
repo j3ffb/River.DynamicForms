328fd0a9cbcd562c3ad505be1740cdbd6fb5fcd1
Jeff Bullock <jbullock@rvc.ac.uk>
2019-11-12 12:40:37 +0000

    Loop elements inside container fields when validating

--- Handlers/FormFileSubmissionCoordinator.cs
+++ Handlers/FormFileSubmissionCoordinator.cs
@@ -14,15 +14,52 @@ namespace River.DynamicForms.Handlers
     {
         public override void Submitted(FormSubmittedEventContext context)
         {
-            SetPathOnFileElements(context, context.Form.Elements);
+            var allFormElements = new List<Element>();
+            HarvestFormElements(context.Form.Elements, allFormElements);
+
+            foreach (var element in allFormElements)
+            {
+                var fileFieldElement = element as FileField;
+
+                if (fileFieldElement == null)
+                    continue;
+
+                var postedFileValue = context.ValueProvider.GetValue(fileFieldElement.Name);
+
+                if (postedFileValue == null)
+                    return;
+
+                var postedFile = ((System.Web.HttpPostedFileBase[])(postedFileValue.RawValue))[0];
+
+                var path = Path.Combine(fileFieldElement.FilePath, Path.GetFileName(postedFile.FileName));
+
+                if (fileFieldElement.GenerateUnique)
+                {
+                    int count = 1;
+                    var pathPattern = Path.Combine(fileFieldElement.FilePath, string.Format("{0}_{{0}}{1}", Path.GetFileNameWithoutExtension(postedFile.FileName), Path.GetExtension(postedFile.FileName)));
+                    while (File.Exists(string.Format(pathPattern, count)))
+                    {
+                        count++;
+                    }
+
+                    path = string.Format(pathPattern, count);
+                }
+
+                context.Values[fileFieldElement.Name + ":size"] = postedFile.ContentLength.ToString();
+                context.Values[fileFieldElement.Name] = path;
+                fileFieldElement.PostedValue = path;
+            }
         }
 
         public override void Validated(FormValidatedEventContext context)
         {
             if (!context.ModelState.IsValid)
             {
+                var allFormElements = new List<Element>();
+                HarvestFormElements(context.Form.Elements, allFormElements);
+
                 //Clean up on validation fail
-                foreach (var element in context.Form.Elements)
+                foreach (var element in allFormElements)
                 {
                     if (element is FileField)
                     {
@@ -39,7 +76,10 @@ namespace River.DynamicForms.Handlers
 
         public override void Validating(FormValidatingEventContext context)
         {
-            foreach (var element in context.Form.Elements)
+            var allFormElements = new List<Element>();
+            HarvestFormElements(context.Form.Elements, allFormElements);
+
+            foreach (var element in allFormElements)
             {
                 var fileFieldElement = element as FileField;
 
@@ -72,46 +112,19 @@ namespace River.DynamicForms.Handlers
                 }
             }
         }
-        private void SetPathOnFileElements(FormSubmittedEventContext context, IList<Element> elements)
+
+        private void HarvestFormElements(IList<Element> parentElements, List<Element> result)
         {
-            foreach (var element in elements)
+            result.AddRange(parentElements);
+
+            foreach (var element in parentElements)
             {
                 var containerElement = element as Container;
                 if (containerElement != null)
                 {
                     // Containers contain other elements, so loop these too.
-                    SetPathOnFileElements(context, containerElement.Elements);
+                    HarvestFormElements(containerElement.Elements, result);
                 }
-
-                var fileFieldElement = element as FileField;
-
-                if (fileFieldElement == null)
-                    continue;
-
-                var postedFileValue = context.ValueProvider.GetValue(fileFieldElement.Name);
-
-                if (postedFileValue == null)
-                    return;
-
-                var postedFile = ((System.Web.HttpPostedFileBase[])(postedFileValue.RawValue))[0];
-
-                var path = Path.Combine(fileFieldElement.FilePath, Path.GetFileName(postedFile.FileName));
-
-                if (fileFieldElement.GenerateUnique)
-                {
-                    int count = 1;
-                    var pathPattern = Path.Combine(fileFieldElement.FilePath, string.Format("{0}_{{0}}{1}", Path.GetFileNameWithoutExtension(postedFile.FileName), Path.GetExtension(postedFile.FileName)));
-                    while (File.Exists(string.Format(pathPattern, count)))
-                    {
-                        count++;
-                    }
-
-                    path = string.Format(pathPattern, count);
-                }
-
-                context.Values[fileFieldElement.Name + ":size"] = postedFile.ContentLength.ToString();
-                context.Values[fileFieldElement.Name] = path;
-                fileFieldElement.PostedValue = path;
             }
         }
-}
+    }
